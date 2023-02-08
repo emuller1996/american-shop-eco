cc123399e8c59ae1c0dfad6e810eefb7954f9ee5
emuller1996 <stefano.muller@hotmail.com>
2023-02-07 23:38:26 -0500

    feat(ProductDetail) add placeholder.

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,154 +1,159 @@
 import axios from 'axios';
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 
-const ProductDetail = () =>{
+const ProductDetail = () => {
     let { id } = useParams();
-    const [productDetail,setProductDetail] = useState();
+    const [productDetail, setProductDetail] = useState(undefined);
+    const history = useHistory();
 
-    useEffect( () => {
+    useEffect(() => {
         getProductDetail(id);
-    },[id])
+    }, [id])
 
-    const getProductDetail = async (id)=>{
-        const result = await axios.get(`http://localhost:3001/Product/${id}`);
+    const getProductDetail = async (id) => {
+        console.log(id);
+
+        const result = await axios.get(`/Products/${id}`);
         console.log(result.data);
         setProductDetail(result.data)
     }
 
 
-    return(
+    return (
         <>
-        <section class="bg-light">
-        <div class="container pb-5">
-            <div class="row">
-                <div class="col-lg-5 mt-5">
-                    
+            <section class="bg-light">
+                <div class="container pb-5">
                     <div class="row">
-                        {/*Start Controls*/}
-                        <div class="col-1 align-self-center">
-                            <a href="#multi-item-example" role="button" data-bs-slide="prev">
-                                <i class="text-dark fas fa-chevron-left"></i>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                        </div>
-                        {/*End Controls*/}
-                        {/*Start Carousel Wrapper*/}
-                        <div id="multi-item-example" class="col-10 carousel slide carousel-multi-item" data-bs-ride="carousel">
-                            {/*Start Slides*/}
-                            <div class="carousel-inner product-links-wap" role="listbox">
+                        <div class="col-lg-5 mt-5">
 
-                                {/*First slide*/}
-                                <div class="carousel-item active">
-                                    <div class="row">
-                                        <div class="col-12">          
-                                            <img class="card-img img-fluid" src={productDetail && productDetail.image} alt="ProductImage1"/>      
+                            <div class="row">
+                                {/*Start Controls*/}
+                                <div class="col-1 align-self-center">
+                                    <a href="#multi-item-example" role="button" data-bs-slide="prev">
+                                        <i class="text-dark fas fa-chevron-left"></i>
+                                        <span class="sr-only">Previous</span>
+                                    </a>
+                                </div>
+                                {/*End Controls*/}
+                                {/*Start Carousel Wrapper*/}
+                                <div id="multi-item-example" class="col-10 carousel slide carousel-multi-item" data-bs-ride="carousel">
+                                    {/*Start Slides*/}
+                                    <div class="carousel-inner product-links-wap" role="listbox">
+
+                                        {/*First slide*/}
+                                        <div class="carousel-item active">
+                                            <div class="row">
+                                                <div class="col-12">
+                                                    <img class="card-img img-fluid" src={productDetail && productDetail.image} alt="ProductImage1" />
+                                                </div>
+                                            </div>
                                         </div>
+
                                     </div>
+                                    {/*End Slides*/}
                                 </div>
-                                
+                                {/*End Carousel Wrapper*/}
+                                {/*Start Controls*/}
+                                <div class="col-1 align-self-center">
+                                    <a href="#multi-item-example" role="button" data-bs-slide="next">
+                                        <i class="text-dark fas fa-chevron-right"></i>
+                                        <span class="sr-only">Next</span>
+                                    </a>
+                                </div>
+                                {/*End Controls*/}
                             </div>
-                            {/*End Slides*/}
                         </div>
-                        {/*End Carousel Wrapper*/}
-                        {/*Start Controls*/}
-                        <div class="col-1 align-self-center">
-                            <a href="#multi-item-example" role="button" data-bs-slide="next">
-                                <i class="text-dark fas fa-chevron-right"></i>
-                                <span class="sr-only">Next</span>
-                            </a>
-                        </div>
-                        {/*End Controls*/}
-                    </div>
-                </div>
-                {/* col end */}
-                <div class="col-lg-7 mt-5">
-                    <div class="card">
-                        <div class="card-body">
-                            <h1 class="h2"> { productDetail ? productDetail.name : ' . . .' } </h1>
-                            <p class="h3 py-2">{ productDetail ? `$ ${productDetail.price}.00` : ' $ 00.' }</p>
-                            <p class="py-2">
-                                <i class="fa fa-star text-warning"></i>
-                                <i class="fa fa-star text-warning"></i>
-                                <i class="fa fa-star text-warning"></i>
-                                <i class="fa fa-star text-warning"></i>
-                                <i class="fa fa-star text-secondary"></i>
-                                <span class="list-inline-item text-dark">Rating 4.8 | 36 Comments</span>
-                            </p>
-                            <ul class="list-inline">
-                                <li class="list-inline-item">
-                                    <h6>Brand: </h6>
-                                </li>
-                                <li class="list-inline-item">
-                                    <p class="text-muted"><strong>{ productDetail ? `${productDetail.brand}` : ' - ' }</strong></p>
-                                </li>
-                            </ul>
+                        {/* col end */}
+                        <div class="col-lg-7 mt-5">
+                            <div class="card">
+                            
+                                <div class="card-body">
+                                <button type="button" onClick={()=> { history.goBack()}} class="btn btn-danger btn-lg float-end" ><i class="fas fa-chevron-left"></i></button>
+                                    <h1 class="h2"> {productDetail ? productDetail.name : <span className="placeholder col-8 rounded"></span>} </h1>
+                                    <p class="h3 py-2  placeholder-glow">{productDetail ? `$ ${productDetail.price}.00` : <span class="placeholder col-3 rounded"></span>}</p>
+                                    <p class="py-2">
+                                        <i class="fa fa-star text-warning"></i>
+                                        <i class="fa fa-star text-warning"></i>
+                                        <i class="fa fa-star text-warning"></i>
+                                        <i class="fa fa-star text-warning"></i>
+                                        <i class="fa fa-star text-secondary"></i>
+                                        <span class="list-inline-item text-dark">Rating 4.8 | 36 Comments</span>
+                                    </p>
+                                    <ul class="list-inline">
+                                        <li class="list-inline-item">
+                                            <h6>Brand: </h6>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <h4 class="text-muted placeholder-glow">{productDetail ? `${productDetail.brand}` : <span class="placeholder col-6"></span>}</h4>
+                                        </li>
+                                    </ul>
 
-                            <h6>Description:</h6>
-                            <p>{ productDetail ? `${productDetail.description}` : ' - ' }</p>
-                            <ul class="list-inline">
-                                <li class="list-inline-item">
-                                    <h6>Avaliable Color :</h6>
-                                </li>
-                                <li class="list-inline-item">
-                                    <p class="text-muted"><strong>White / Black</strong></p>
-                                </li>
-                            </ul>
+                                    <h6>Description:</h6>
+                                    <p className='placeholder-glow '>{productDetail ? `${productDetail.description}` :
+                                        <>
+                                            <span class="placeholder col-5 rounded"></span>
+                                            <span class="placeholder col-7 rounded"></span>
+                                            <span class="placeholder col-4 rounded"></span>
+                                            <span class="placeholder col-6 rounded"></span>
+                                            <span class="placeholder col-8 rounded"></span>
+                                            <span class="placeholder col-4 rounded"></span>
 
-                            <h6>Specification:</h6>
-                            <ul class="list-unstyled pb-3">
-                                <li>Lorem ipsum dolor sit</li>
-                                <li>Amet, consectetur</li>
-                                <li>Adipiscing elit,set</li>
-                                <li>Duis aute irure</li>
-                                <li>Ut enim ad minim</li>
-                                <li>Dolore magna aliqua</li>
-                                <li>Excepteur sint</li>
-                            </ul>
+                                        </>}</p>
+                                    <ul class="list-inline">
+                                        <li class="list-inline-item">
+                                            <h6>Avaliable Color :</h6>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <p class="text-muted"><strong>White / Black</strong></p>
+                                        </li>
+                                    </ul>
 
-                            <form action="" method="GET">
-                                <input type="hidden" name="product-title" value="Activewear"/>
-                                <div class="row">
-                                    <div class="col-auto">
-                                        <ul class="list-inline pb-3">
-                                            <li class="list-inline-item">Size :
-                                                <input type="hidden" name="product-size" id="product-size" value="S"/>
-                                            </li>
-                                            <li class="list-inline-item"><span class="btn btn-success btn-size">S</span></li>
-                                            <li class="list-inline-item"><span class="btn btn-success btn-size">M</span></li>
-                                            <li class="list-inline-item"><span class="btn btn-success btn-size">L</span></li>
-                                            <li class="list-inline-item"><span class="btn btn-success btn-size">XL</span></li>
-                                        </ul>
-                                    </div>
-                                    <div class="col-auto">
-                                        <ul class="list-inline pb-3">
-                                            <li class="list-inline-item text-right">
-                                                Quantity
-                                                <input type="hidden" name="product-quanity" id="product-quanity" value="1"/>
-                                            </li>
-                                            <li class="list-inline-item"><span class="btn btn-success" id="btn-minus">-</span></li>
-                                            <li class="list-inline-item"><span class="badge bg-secondary" id="var-value">1</span></li>
-                                            <li class="list-inline-item"><span class="btn btn-success" id="btn-plus">+</span></li>
-                                        </ul>
-                                    </div>
+                                    
+
+                                    <form action="" method="GET">
+                                        <input type="hidden" name="product-title" value="Activewear" />
+                                        <div class="row">
+                                            <div class="col-auto">
+                                                <ul class="list-inline pb-3">
+                                                    <li class="list-inline-item">Size :
+                                                        <input type="hidden" name="product-size" id="product-size" value="S" />
+                                                    </li>
+                                                    <li class="list-inline-item"><span class="btn btn-danger btn-size">S</span></li>
+                                                    <li class="list-inline-item"><span class="btn btn-danger btn-size">M</span></li>
+                                                    <li class="list-inline-item"><span class="btn btn-danger btn-size">L</span></li>
+                                                    <li class="list-inline-item"><span class="btn btn-danger btn-size">XL</span></li>
+                                                </ul>
+                                            </div>
+                                            <div class="col-auto">
+                                                <ul class="list-inline pb-3">
+                                                    <li class="list-inline-item text-right">
+                                                        Quantity
+                                                        <input type="hidden" name="product-quanity" id="product-quanity" value="1" />
+                                                    </li>
+                                                    <li class="list-inline-item"><span class="btn btn-danger" id="btn-minus">-</span></li>
+                                                    <li class="list-inline-item"><span class="badge bg-secondary" id="var-value">1</span></li>
+                                                    <li class="list-inline-item"><span class="btn btn-danger" id="btn-plus">+</span></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="row pb-3">
+                                            <div class="col d-grid">
+                                                <button type="submit" class="btn btn-danger btn-lg" name="submit" value="buy">Buy</button>
+                                            </div>
+                                            <div class="col d-grid">
+                                                <button type="submit" class="btn btn-danger btn-lg" name="submit" value="addtocard">Add To Cart</button>
+                                            </div>
+                                        </div>
+                                    </form>
+
                                 </div>
-                                <div class="row pb-3">
-                                    <div class="col d-grid">
-                                        <button type="submit" class="btn btn-success btn-lg" name="submit" value="buy">Buy</button>
-                                    </div>
-                                    <div class="col d-grid">
-                                        <button type="submit" class="btn btn-success btn-lg" name="submit" value="addtocard">Add To Cart</button>
-                                    </div>
-                                </div>
-                            </form>
-
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </section>
+            </section>
         </>
     )
 }
