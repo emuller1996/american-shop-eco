11d6d8ab6d57c10d68eeacced1808ca2f8b17ccd
emuller1996 <stefano.muller@hotmail.com>
2023-02-09 17:07:17 -0500

    feat add my cart with redux

--- src/features/Car/carSlice.js
+++ src/features/Car/carSlice.js
@@ -3,27 +3,22 @@ import { createSlice } from '@reduxjs/toolkit'
 
 export const carSlice = createSlice({
   name: 'cart',
-  initialState : {
-    cart:[],
-    totalCart:0,
-  },
+  initialState : [],
   reducers: {
-    setCart: (state,action) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.cart = action.payload
+    addProductToCart: (state,action) => {
+      if(!state.includes( p => p.id !== action.payload.id )){
+        return [...state, action.payload]
+      }
     },
-    setTotalCart:(state,action)=>{
-
-      state.totalCart = action.payload
+    deleteProductsToCart: ( state , action) => {
+      console.log(action.payload)
+      return [ ...state.filter( p => p.id !== action.payload)]
     }
-    
+  
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { setCart, setTotalCart } = carSlice.actions
+export const { addProductToCart, deleteProductsToCart } = carSlice.actions
 
 export default carSlice.reducer
