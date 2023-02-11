dfe12b1224d88aa36a6770d610b1cbd26f390a81
emuller1996 <stefano.muller@hotmail.com>
2023-02-11 17:11:47 -0500

    add auth0 login

---
+++ src/components/Nav/DropdownProfile.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import LogoutButton from "./LogoutButton";
+import { Dropdown, DropdownButton } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+export default function DropdownProfile() {
+  const { user } = useAuth0();
+  console.log(user);
+  return (
+    <>
+      <Dropdown>
+        <Dropdown.Toggle variant="white" id="dropdown-basic">
+          <i className="fa fa-fw fa-user text-dark mr-3"></i>
+        </Dropdown.Toggle>
+        <Dropdown.Menu id="example-fade-text">
+          <Dropdown.ItemText>
+            <div className="container bg-ligth border rounded-3 p-2">
+              <div className="text-center">
+                <img
+                  src={user ? user.picture : "..."}
+                  width="60px"
+                  class="img-fluid rounded-pill mb-2"
+                  alt="..."
+                />
+              </div>
+              <small className="text-sm p-0 m-0 f-6 text-nowrap text-uppercase d-block">
+                {user ? user.name : "..."}
+              </small>
+             
+              <small className="text-sm p-0 m-0 f-6 text-nowrap text-uppercase d-block mt-2">
+              <LogoutButton />
+              </small>
+            </div>
+          </Dropdown.ItemText>
+          <Dropdown.Item href="#/action-2">
+            <Link to={"/Dashboard"}>Dasboard</Link>
+          </Dropdown.Item>
+          <Dropdown.Item href="#/action-3">
+            
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { store } from './app/store'
-import { Provider } from 'react-redux'
-import axios from 'axios';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import axios from "axios";
+import { Auth0Provider } from "@auth0/auth0-react";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const  { REACT_APP_AUTH0_DOMAIN, REACT_APP_AUTH0_CLIENT_ID} = process.env;
 
 axios.defaults.baseURL = process.env.REACT_APP_API;
 root.render(
-  
   /* <React.StrictMode> */
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <Auth0Provider 
+  domain={REACT_APP_AUTH0_DOMAIN} 
+  clientId={REACT_APP_AUTH0_CLIENT_ID}
+  /* audience={REACT_APP_AUTH0_AUDIENCE}   */
+  redirectUri={window.location.origin}
+  authorizationParams={{
+    redirect_uri: window.location.origin
+  }} >
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Auth0Provider>
 
   /* </React.StrictMode> */
 );
