61e0ef346330b3b97410d3c91662cde64707be7e
emuller1996 <stefano.muller@hotmail.com>
2023-03-04 09:56:39 -0500

    feat(DeliveryAddress) se añadio listar dirrecion por usuario en su perfil

---
+++ src/components/Dashboard/MyProfile/FormDeliveryAddress.jsx
@@ -0,0 +1,113 @@
+import React,{ useState } from "react";
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+export default function FormDeliverAddressComponent({ handleClose }) {
+
+
+    const [deliveryAddress,setDeliverAddress] = useState(undefined);
+
+    const onHandleSubmit = (e) => {
+        e.preventDefault();
+        console.log("onHandleSubmit")
+        console.log(deliveryAddress)
+
+    }
+
+
+    const onHandleInput = (e) => {
+        setDeliverAddress({
+            ...deliveryAddress,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    return (
+        <>
+
+            <form onSubmit={onHandleSubmit} action="">
+                <Modal.Body>
+
+                    <div class="form-floating mb-3">
+                        <input type="text"
+                            class="form-control"
+                            id="name"
+                            name="name"
+                            onChange={onHandleInput}
+                        />
+                        <label htmlFor="name">Nombre</label>
+                    </div>
+
+                    <div class="form-floating mb-3">
+                        <input type="text"
+                            class="form-control"
+                            id="departament"
+                            name="departament"
+                            onChange={onHandleInput}
+                        />
+                        <label htmlFor="departament">Departamento</label>
+                    </div>
+
+                    <div class="form-floating mb-3">
+                        <input type="text"
+                            class="form-control"
+                            id="city"
+                            name="city"
+                            onChange={onHandleInput}
+                        />
+                        <label htmlFor="city">Ciudad</label>
+                    </div>
+
+                    <div class="form-floating mb-3">
+                        <input type="text"
+                            class="form-control"
+                            id="address"
+                            name="address"
+                            onChange={onHandleInput}
+                        />
+                        <label htmlFor="address">Dirrecion</label>
+                    </div>
+
+                    <div class="form-floating mb-3">
+                        <input type="text"
+                            class="form-control"
+                            id="neighborhood"
+                            name="neighborhood"
+                            onChange={onHandleInput}
+                        />
+                        <label htmlFor="neighborhood">Barrio</label>
+                    </div>
+
+                    <div class="form-floating mb-3">
+                        <input type="text"
+                            class="form-control"
+                            id="phone"
+                            name="phone"
+                            onChange={onHandleInput}
+                        />
+                        <label htmlFor="phone">Telefono</label>
+                    </div>
+
+                    <div class="form-floating mb-3">
+                        <input type="text"
+                            class="form-control"
+                            id="reference"
+                            name="reference"
+                            onChange={onHandleInput}
+                        />
+                        <label htmlFor="reference">Referencia</label>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Cerrar
+                    </Button>
+                    <Button variant="primary" type="submit" onClick={handleClose}>
+                        Guardar
+                    </Button>
+                </Modal.Footer>
+            </form>
+
+        </>
+    )
+}
--- src/components/Dashboard/MyProfile/MyDeliveryAddress.jsx
+++ src/components/Dashboard/MyProfile/MyDeliveryAddress.jsx
@@ -1,38 +1,95 @@
-import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import FormDeliverAddressComponent from "./FormDeliveryAddress";
+import Spinner from 'react-bootstrap/Spinner';
+
 
 export default function MyDeliveryAddressComponent() {
+
+  const [deliveryAddress, setDeliverAddress] = useState(undefined);
+  const { user } = useAuth0();
+  useEffect(() => {
+
+    getDeliverAddress();
+
+  }, [])
+
+  const getDeliverAddress = async () => {
+
+
+    try {
+      const result = await axios.get(`/deliveryAddress/${user.email}`)
+      console.log(result.data[0].DeliveryAddresses)
+      setDeliverAddress(result.data[0].DeliveryAddresses)
+
+    } catch (error) {
+
+
+    }
+
+  }
+
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div className="mt-3">
       <div className="card">
         <div className="card-body">
           <p className="fw-semibold fs-5">Mis Direciones de Envio</p>
-
+          <Button variant="danger" className="mb-3" onClick={handleShow}>
+            Agregar Nueva Direccion.
+          </Button>
           <div className="row">
-            <div className="col-12">
-              <div className="card p-2">
-                <p className="p-0 m-0 fw-semibold">Mi Casa </p>
-                <div className="row">
+
+            {deliveryAddress && deliveryAddress.length === 0 && (<p> No Tiene Direcciones Guardas </p>)}
+            {deliveryAddress ? deliveryAddress.map(c => (
+              <div className="col-12 mb-3">
+                <div className="card p-2">
+                  <p className="p-0 m-0 fw-semibold">Mi Casa </p>
+                  <div className="row">
                     <div className="col">
-                        Direccion : Cra 13 ° N #3 a 13
+                      Direccion : {c.address}
+                    </div>
+
+                    <div className="col">
+                      Barrio : {c.neighborhood}
                     </div>
                     <div className="col">
-                        Apt : 201
+                      Cudiad : {c.city}
                     </div>
                     <div className="col">
-                        Barrio : Bellavista
+                      Telefono : {c.phone}
                     </div>
-                    <div className="col">
-                        Cudiad : Buevantetura
-                    </div>
-                    <div className="col">
-                        Telefono : 3184612011
-                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
+            )) : (<div className="col-12 text-center p-4">
+              <Spinner animation="border" variant="danger" role="danger">
+                <span className="visually-hidden">Loading...</span>
+              </Spinner>
+            </div>)}
+
+
           </div>
         </div>
       </div>
+
+
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Nueva Direccion</Modal.Title>
+        </Modal.Header>
+
+        <FormDeliverAddressComponent handleClose={handleClose} />
+      </Modal>
     </div>
   );
 }
