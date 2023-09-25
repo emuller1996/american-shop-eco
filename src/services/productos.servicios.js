d25c19b44281c65e5f93cfbf477ebcfbd38b8080
emuller1996 <stefano.muller@hotmail.com>
2023-09-25 16:33:39 -0500

    hot fix

--- src/services/productos.servicios.js
+++ src/services/productos.servicios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getProductByIdServicio = (id, token) => {
   return axios.get(`/Products/${id}`, {
     headers: {
-      Autarization: `Bearber ${token}`,
+      Authorization: `Bearer ${token}`,
     },
   });
 };
--- src/utils/ProtectedRouteMiProfile.jsx
+++ src/utils/ProtectedRouteMiProfile.jsx
@@ -1,6 +1,5 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
-import { Navigate } from "react-router-dom";
 import { Redirect, Route } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function ProtectedRouteMiProfile({ children, ...rest }) {
