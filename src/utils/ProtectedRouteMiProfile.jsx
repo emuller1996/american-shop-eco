7b74ad0c48c4c31ce9f723731b07b7d1b8a8d96f
emuller1996 <stefano.muller@hotmail.com>
2023-10-10 12:33:25 -0500

    fix proteccion de rutas

--- src/utils/ProtectedRouteMiProfile.jsx
+++ src/utils/ProtectedRouteMiProfile.jsx
@@ -1,9 +1,18 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import { Redirect, Route } from "react-router-dom/cjs/react-router-dom.min";
+import SpinnerComponent from "../components/Spinner";
 
 export default function ProtectedRouteMiProfile({ children, ...rest }) {
-  let { user } = useAuth0();
+  let { user, isLoading } = useAuth0();
+
+  if (isLoading)
+    return (
+      <div>
+        <SpinnerComponent />{" "}
+      </div>
+    );
+
   return (
     <Route
       {...rest}
