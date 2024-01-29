7281d752c052875438f48c032e5b1bea9c60c6d9
emuller1996 <stefano.muller@hotmail.com>
2024-01-29 01:33:48 -0500

    otro propuets de las cards products

--- src/pages/Shop/CardProduct.jsx
+++ src/pages/Shop/CardProduct.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { MostrarPesoCOP } from "../../utils";
 
 export default function CardProduct({ product, addProducToCart }) {
   return (
     <div className="col-md-4" data-aos="fade-out" data-aos-duration="300">
-      <Link
+      {/* <Link
         className="card card-product mb-4 product-wap rounded-0 text-decoration-none h-100"
         to={`/articulo/${product.id}`}
       >
@@ -24,17 +25,11 @@ export default function CardProduct({ product, addProducToCart }) {
               <li>
                 <span
                   title="Vista Rapida"
-                  /* onClick={( e) => e.preventDefault()} */ className="btn btn-light rounded-4 text-danger"
+                  className="btn btn-light rounded-4 text-danger"
                 >
                   <i className="fas fa-eye"></i>
                 </span>
               </li>
-
-              {/* <li onClick={(e, id) => addProducToCart(e, product.id)}>
-                <span className="btn btn-dark text-white mt-2">
-                  <i className="fas fa-cart-plus"></i>
-                </span>
-              </li> */}
             </ul>
           </div>
         </div>
@@ -50,21 +45,6 @@ export default function CardProduct({ product, addProducToCart }) {
           <p className="text-center m-0 fs-6 placeholder-glow">
             {product.Category.name}
           </p>
-
-          {/* <ul className="list-unstyled d-flex justify-content-center mb-1">
-            <li>
-              {[1, 2, 3, 4, 5].map((n) => {
-                if (n <= product.rating)
-                  return <i key={n} className="text-warning fa fa-star"></i>;
-                else return <i key={n} className="text-muted fa fa-star"></i>;
-              })}
-              <i className="text-warning fa fa-star"></i>
-                            <i className="text-warning fa fa-star"></i>
-                            <i className="text-warning fa fa-star"></i>
-                            <i className="text-muted fa fa-star"></i>
-                            <i className="text-muted fa fa-star"></i>
-            </li>
-          </ul> */}
           <p className="text-center mb-0 text-danger fw-normal">
             $
             {`${product.price.toLocaleString(undefined, {
@@ -72,7 +52,26 @@ export default function CardProduct({ product, addProducToCart }) {
             })}`}
           </p>
         </div>
+      </Link> */}
+      <Link  to={`/articulo/${product.id}`} class="card">
+        <div class="image">
+          <img
+          height={"400px"}
+          className="img-product"
+            src={
+              product.image
+                ? product.image
+                : "https://nayemdevs.com/wp-content/uploads/2020/03/default-product-image.png"
+            }
+            alt=""
+          />          
+        </div>
+        <div class="text">
+          <span>{product.name}</span>
+          <p>{MostrarPesoCOP(product.price)}</p>
+        </div>
       </Link>
+
     </div>
   );
 }
