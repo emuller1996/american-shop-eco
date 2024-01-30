c9e24159c5ef2ba494f01e594afdfde62f663779
emuller1996 <stefano.muller@hotmail.com>
2024-01-30 00:34:29 -0500

    change filter categorias

--- src/pages/Shop/FilterProducts.jsx
+++ src/pages/Shop/FilterProducts.jsx
@@ -8,10 +8,9 @@ export default function FilterProducts({ categories, setCategoryFilter }) {
   const dispatch = useDispatch();
   const category = useSelector((state) => state.products.category);
 
-
   return (
     <>
-      <Accordion defaultActiveKey="0" className="border-0" flush alwaysOpen>
+      {/* <Accordion defaultActiveKey="0" className="border-0" flush alwaysOpen>
         <Accordion.Item eventKey="0"  className="border-0 rounded-3">
           <Accordion.Header  className="">Filtar por Categorias</Accordion.Header>
           <Accordion.Body>
@@ -40,58 +39,31 @@ export default function FilterProducts({ categories, setCategoryFilter }) {
               ))}
           </Accordion.Body>
         </Accordion.Item>
-      </Accordion>
-
-      {/* <div
-        className="accordion list-unstyled templatemo-accordion  "
-        id="accordionExample"
-      >
-        <div className="accordion-item pb-3 border-0">
-          <h2 className="accordion-header" id="headingOne">
-            <div
-              className="d-flex justify-content-between h3 text-decoration-none"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseOne"
-              aria-expanded="true"
-              aria-controls="collapseOne"
-            >
-              Filtar por Categorias
-              <i className="fa fa-fw fa-chevron-circle-down mt-1"></i>
-            </div>
-          </h2>
-          <div
-            id="collapseOne"
-            className="accordion-collapse collapse show"
-            aria-labelledby="headingOne"
-            data-bs-parent="#accordionExample"
-          >
-            <div className="accordion-body">
-              <Form.Check
-                key={99}
-                type="radio"
-                value={""}
-                name={"category"}
-                id={99}
-                label={"All"}
-                onChange={(e) => setCategoryFilter(e.target.value)}
-              />
-              {categories &&
-                categories.map((c) => (
-                  <Form.Check
-                    key={c.id}
-                    type="radio"
-                    value={c.id}
-                    name={"category"}
-                    id={c.id}
-                    label={c.name}
-                    onChange={(e) => setCategoryFilter(e.target.value)}
-                  />
-                ))}
-            </div>
-          </div>
+      </Accordion> */}
+      <div className="card-filtar p-4 rounded-4  mb-4">
+        <h3 className="mb-3 h6 text-danger  text-uppercase  d-block">
+          Categorias
+        </h3>
+        <div className="d-flex flex-column gap-1 ">
+          {categories &&
+            categories.map((c) => (
+              <>
+                <label className="label_filter_check d-flex justify-content-between ">
+                  <div>
+                    <input
+                      value={c.id}
+                      type="radio"
+                      name={"category"}
+                      onChange={(e) => dispatch(setCategory(e.target.value))}
+                    />
+                    <span>{c.name}</span>
+                  </div>
+                  <b>{c.productCantidad}</b>
+                </label>
+              </>
+            ))}
         </div>
-      </div> */}
+      </div>
     </>
   );
 }
